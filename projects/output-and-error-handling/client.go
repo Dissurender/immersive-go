7ee0dd9cbc086689dd16a0e4008d698cdcf28fa1
Diss <65290988+Dissurender@users.noreply.github.com>
2023-11-23 07:32:53 -0600

    working con

--- projects/output-and-error-handling/client.go
+++ projects/output-and-error-handling/client.go
@@ -13,16 +13,20 @@ func main() {
 	client := &http.Client{Timeout: 10 * time.Second}
 
 	for i := 0; i < 3; i++ {
+		time.Sleep(time.Second * 1)
 		resp, err := client.Get("http://localhost:8080")
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Status Code: %d", resp.StatusCode)
 			fmt.Fprintf(os.Stderr, "Error occured while making request: %s", err)
-			os.Exit(1)
+			fmt.Fprintln(os.Stderr, "\nRetrying fetch..")
+			i--
+			continue
 		}
 
-		retry := handleResp(resp)
-		if retry != nil {
+		e := handleResp(resp)
+		if e != nil {
+			fmt.Fprintln(os.Stderr, e.Error())
 		}
+
 	}
 
 }
@@ -38,31 +42,39 @@ func handleResp(res *http.Response) error {
 		bodyBytes, _ := io.ReadAll(res.Body)
 		body := string(bodyBytes)
 		fmt.Println(body)
-		fmt.Println("Server response ->", string(body))
+		// fmt.Println("Server response ->", string(body))
 
 	case 429:
 		retryAfter := res.Header.Get("Retry-After")
 		if retryAfter != "" {
 			if seconds, err := strconv.Atoi(retryAfter); err == nil {
-				fmt.Fprintf(os.Stderr, "Server busy, waiting for %d seconds.", seconds)
+
+				if seconds > 5 {
+					return fmt.Errorf("Server too busy, please try again later")
+				}
+
+				fmt.Fprintf(os.Stderr, "Server busy, waiting for %d seconds.\n", seconds)
 				time.Sleep(time.Duration(seconds) * time.Second)
 				fmt.Println("retrying...")
 			} else if retryTime, err := http.ParseTime(retryAfter); err == nil {
 
 				wait := time.Until(retryTime)
+				if wait > 5 {
+					return fmt.Errorf("Server too busy, please try again later")
+				}
+
 				if wait > 0 {
 					fmt.Fprintf(os.Stderr, "Server busy, waiting until %v to retry.", retryTime)
 					time.Sleep(wait)
 					fmt.Println("retrying...")
 				}
-			} else {
-				fmt.Fprintln(os.Stderr, "Retry-After value not valid.")
-				return fmt.Errorf("Retry-After value not valid.")
 
+			} else {
+				return fmt.Errorf("Retry-After value not valid")
 			}
 		} else {
 			fmt.Fprintln(os.Stderr, "No Retry-After property found.")
-			return fmt.Errorf("No Retry-After property found.")
+			return fmt.Errorf("No Retry-After property found")
 		}
 
 	default:
